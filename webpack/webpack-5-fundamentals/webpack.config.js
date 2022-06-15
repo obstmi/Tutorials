7dd3e6884a4d42a722e36108f9231a6e7581f3ab
Michael Obst <michael.obst@telekom.de>
2022-06-15 17:47:19 +0200

    New webpack-5 fundamentals tutorial

---
+++ webpack/webpack-5-fundamentals/webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {
+      static: './dist'
+  },
+  plugins: [
+      new HtmlWebpackPlugin({
+          title: 'Development'
+      })
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
